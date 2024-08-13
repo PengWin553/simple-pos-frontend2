efbf116639154f024d1da06f52f0dbbc3f416510
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-08-13 12:30:30 +0800

    Add gif and changed title text of Home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,18 @@
 const Home = () => {
-    return(
+    return (
         <div className="crud-content-container">
-            <h2 className="page-title">Welcome to The Homepage</h2>
+            <h2 className="page-title">ホームページへようこそ、Pengwin Kobayashiさん!</h2>
+
+            <center>
+                <img 
+                    src="https://c.tenor.com/5aZ8jrEPWfQAAAAM/kobayashi.gif" 
+                    alt="Kobayashi Gif" 
+                    style={{ width: '300px', height: 'auto' }} // Adjust the width and height as needed
+                />
+            </center>
+         
         </div>
-    )
+    );
 }
 
 export default Home;
